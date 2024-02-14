9092eb3ff74512b55dc1737b98e4c3cbad749515
dev_alina <a_srymova@gosreestr.kz>
2024-02-14 17:22:09 +0600

    Added pipeline for logging and middleware for error handling

--- src/API/Private/BookTracker.BusinessLogic/Extensions/ServiceCollectionExtensions.cs
+++ src/API/Private/BookTracker.BusinessLogic/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using BookTracker.BusinessLogic.Services.Password;
+﻿using BookTracker.BusinessLogic.Behaviors;
+using BookTracker.BusinessLogic.Services.Password;
 using Mediator;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
@@ -11,7 +12,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
     {
         services.AddMediator(options => options.ServiceLifetime = ServiceLifetime.Singleton);
-        //services.AddSingleton<typeof(IPipelineBehavior<TMessage,TResponse>)>()
+        services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         services.AddSingleton<IPasswordService, PasswordService>();
         return services; 
     }
